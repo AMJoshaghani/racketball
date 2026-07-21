59e2012fde0829752739f02e63911148b891ccd9
AMJoshaghani <amjoshaghani@gmail.com>
2026-07-21 21:21:03 +0330

    fix TURN server

--- js/network.js
+++ js/network.js
@@ -4,11 +4,30 @@
 // ball simulates it" scheme, which was fragile and duplicated logic.
 
 const ICE_SERVERS = [
-  { urls: 'stun:stun.relay.metered.ca:80' },
-  { urls: 'turn:global.relay.metered.ca:80', username: 'openrelayproject', credential: 'openrelayproject' },
-  { urls: 'turn:global.relay.metered.ca:443', username: 'openrelayproject', credential: 'openrelayproject' },
-  { urls: 'turn:global.relay.metered.ca:443?transport=tcp', username: 'openrelayproject', credential: 'openrelayproject' },
-];
+      {
+        urls: "stun:stun.relay.metered.ca:80",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:80",
+        username: "665f6f8956fc5ffafe663022",
+        credential: "CVAZ/dYHQwNEN7BX",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:80?transport=tcp",
+        username: "665f6f8956fc5ffafe663022",
+        credential: "CVAZ/dYHQwNEN7BX",
+      },
+      {
+        urls: "turn:global.relay.metered.ca:443",
+        username: "665f6f8956fc5ffafe663022",
+        credential: "CVAZ/dYHQwNEN7BX",
+      },
+      {
+        urls: "turns:global.relay.metered.ca:443?transport=tcp",
+        username: "665f6f8956fc5ffafe663022",
+        credential: "CVAZ/dYHQwNEN7BX",
+      },
+  ];
 
 export class NetSession {
   constructor() {
